e06bfd37fe8e1063705cb3f40bd89313bfd37a84
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-10-23 21:41:03 +0200

    updated

--- Tehtävä-1/osa-1.py
+++ Tehtävä-1/osa-1.py
@@ -5,5 +5,11 @@ class Car:
         self.register_number = register_number
         self.max_speed = max_speed
 
+    def accelerate(self, speed_change):
+        if speed_change > 0:
+            self.current_speed = max(self.current_speed + speed_change)
+        else:
+            self.current_speed = min(self.current_speed + speed_change)
+
 new_car = Car ("ABC-123 ", 142)
-print(f" Tä auton rekisteritunnus on: {new_car.register_number}\n Huippunopeus on: {new_car.max_speed}km/h.")
+print(f"Tää autoon rekisterinumero on:{new_car.register_number}","\nAuton nopeus on:",{new_car.max_speed})
