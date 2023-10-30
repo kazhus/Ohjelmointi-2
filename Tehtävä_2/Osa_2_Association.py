ea118ccbb5eede72a1915424d2d6090b9ac496e7
hussainkazemian <142801412+hussainkazemian@users.noreply.github.com>
2023-10-30 09:55:22 +0200

    updated

---
+++ Tehtävä_2/Osa_2_Association.py
@@ -0,0 +1,10 @@
+class Elevator:
+    pass
+def __init__(self,kerros_ylös, kerros_alas)
+    self.kerros_alas = kerros_alas
+    self.kerros_ylös = kerros_ylös
+    self.current_kerros = kerros_alas
+
+    def go_to_floor(self, floor):
+        if floor > self.current_kerros:
+            self.kerros
